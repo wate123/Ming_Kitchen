54066b3a1aceff47a49124ecc2aa4f6ff6a9fc91
wate123 <junlin0604@gmail.com>
2021-12-31 18:17:22 -0600

    custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+export default function Custom404() {
+  return (
+    <div style={{ background: "black" }}>
+      <img
+        style={{
+          height: "100vh",
+          display: "block",
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+        src="/404.jpeg"
+      />
+    </div>
+  );
+}
