9282d79f6c04b606572afd139c2296e8ec3c0be8
wate123 <junlin0604@gmail.com>
2022-02-19 15:32:47 -0800

    image optimize

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Image from "next/image";
+import Head from "../../public/Head.jpg";
 import styles from "/styles/Header.module.css";
 export default function index() {
   return (
-    <div>
-      <img className={styles.head} src="/Head.jpg" alt="Ming Kitchen" />
+    <div style={{ height: "250px", position: "relative" }}>
+      {/* <img className={styles.head} src="/Head.jpg" alt="Ming Kitchen" /> */}
+      <Image
+        // className={styles.head}
+        src={Head}
+        // height={300}
+        priority
+        layout="fill"
+        objectFit="cover"
+        alt="Ming Kitchen"
+      />
     </div>
   );
 }
--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,5 @@
 import styles from "/styles/404.module.css";
+
 export default function Custom404() {
   return (
     <div className={styles.custom404}>
